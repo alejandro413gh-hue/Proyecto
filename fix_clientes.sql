b338475df703e5ea6b3d6958557578877d16f4e6
alejandro413gh-hue <alejandro413gh@gmail.com>
2026-06-03 07:03:02 -0500

    actualización

--- fix_clientes.sql
+++
@@ -1,3 +0,0 @@
--- Add missing columns to clientes table
-ALTER TABLE clientes ADD COLUMN nit VARCHAR(50) DEFAULT NULL AFTER nombre;
-ALTER TABLE clientes ADD COLUMN factura VARCHAR(100) DEFAULT NULL AFTER direccion;
